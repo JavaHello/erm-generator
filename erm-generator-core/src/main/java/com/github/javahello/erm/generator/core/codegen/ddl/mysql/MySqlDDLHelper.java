b575d34e1935ba42a386230ffa9ad91b992d16d6
kaiv2 <kaiv2@outlook.com>
2021-09-05 00:05:23 +0800

    warpComment

--- erm-generator-core/src/main/java/com/github/javahello/erm/generator/core/codegen/ddl/mysql/MySqlDDLHelper.java
+++ erm-generator-core/src/main/java/com/github/javahello/erm/generator/core/codegen/ddl/mysql/MySqlDDLHelper.java
@@ -34,7 +34,14 @@ public abstract class MySqlDDLHelper {
         if (DiffHelper.isEmpty(column.getColumnComment())) {
             return "";
         }
-        return " COMMENT " + warp(column.getColumnComment(), "'");
+        return " COMMENT " + warpComment(column.getColumnComment());
+    }
+
+    private static String warpComment(String comment) {
+        if (comment.contains("'")) {
+            comment = comment.replaceAll("'", "''");
+        }
+        return warp(comment, "'");
     }
 
 
